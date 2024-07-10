337c6b54a4c08f604c10b3cb55f7cf1da64f72ae
NGO TRONG LIEM <ngotrongliem2004@gmail.com>
2024-07-10 20:07:31 +0700

    học Seeders fake dữ liệu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('products')->insert([
+            'name_product' => Str::random(10),
+            'image' => Str::random(15),
+            'price' => rand(100, 1000),
+            'description' =>  Str::random(50),
+            'status' => '1',
+            'created_at' => now(),
+            'updated_at' => now(), 
+        ]);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@localhost',
+            // 'email_verified_at'=> 'email.verified_at@gmail.com',
+            'password' => Hash::make('admin'),
+            'remember_token' => Str::random(18),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
