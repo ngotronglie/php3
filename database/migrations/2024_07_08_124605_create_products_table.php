ac47e31bbbd39c644c5b9fe38f34b3fceaa01250
NGO TRONG LIEM <ngotrongliem2004@gmail.com>
2024-07-12 19:42:09 +0700

    query buider laravel

--- database/migrations/2024_07_08_124605_create_products_table.php
+++ database/migrations/2024_07_08_124605_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->double('price', 10, 2); // phan truoc toi da 10 so , phan sau dau phay co 2 so
             $table->text('description');
             $table->boolean('status')->default(true);
+            $table->integer('category_id');
             $table->timestamps();  // created_at, updated_at 
 
         });
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,76 @@ Route::get('/hihi', function () {
 });
 Route::get('/categories', function () {
 //    $data = DB::table('categories')->get();  // lấy ra tất cả bản ghi trong bảng categories
-   $data = DB::table('categories')->first(); // lọc ra bản ghi đầu tiên trong bảng categories
+//    $data = DB::table('categories')->first(); // lọc ra bản ghi đầu tiên trong bảng categories
+
+        // $data = DB::table('categories')
+        //     -> where('id', 1)->get(); // lấy category co id = 1 trong bảng categories
+            // ->toRawSql(); xem dữ liệu sql
+
+
+        // $data  = DB::table('categories')
+        //     ->select('id', 'name')->get();  // chỉ lấy cột id và name show ra
+
+
+
+        // like
+
+        // $data = DB::table('categories')
+        //     ->where('name', 'like', '%lie%')->get();    
+
+        // toán tử 
+
+        // $data = DB::table('categories')
+        //     ->where('id', '>=', 2)->get();   // => id =2
+
+
+
+        // and
+
+            //   c1: 
+
+            // $data = DB::table('categories')
+            //     ->where('id', '>=', 1)
+            //     ->where('name', 'like', '%lie%')->get();
+            // c2:
+                // $data = DB::table('categories')
+                // ->where([
+                //     ['id', '>=', 1],
+                //     ['name', 'like', '%lie%']
+                // ])->get();
+
+
+
+        // or 
+
+            // $data = DB::table('categories')
+            //     -> where('id', '>=', 3)
+            //     ->orWhere('name', 'like', '%a%')->get()->toArray();
+
+            // ===========================================================================================================
+
+        // whereBetween , whereNotBetween 
+        // whereDate , whereMonth , whereYear , whereTime , whereDay 
+
+            // ===========================================================================================================
+
+        // join , leftjoin , rightjoin 
+
+        // $data = DB::table('categories')
+        //     ->join('products', 'categories.id', '=', 'products.categories_id')
+        //     // ->select('categories.name as category_name', 'products.name as product_name')
+        //     ->get();
+
+        // $data = DB::table('categories')
+        // ->leftjoin('products', 'categories.id', '=', 'products.categories_id')
+        // select('categories.name as category_name', 'products.nameelect('categories.*', 'products.name_product as product_name')
+        // ->get();
+
+        $data = DB::table('categories')
+        ->rightJoin('products', 'categories.id', '=', 'products.categories_id')
+        -> select('categories.*', 'products.name_product as product_name')
+        ->get();
+
     dd($data);
     return $data;
 });
