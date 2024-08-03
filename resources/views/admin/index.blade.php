9e46d32afca6d2965f0af31da8270b62e00a77e7
NGO TRONG LIEM <ngotrongliem2004@gmail.com>
2024-08-03 15:58:47 +0700

    create  migration and cuting layouts

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,35 +1,5 @@
-<!doctype html>
-<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
-    data-sidebar-image="none" data-preloader="disable">
 
-<head>
-    <meta charset="utf-8" />
-    <title>Dashboard | Velzon - Admin & Dashboard Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('theme/admin/assets/images/favicon.ico') }}">
-
-    <!-- jsvectormap css -->
-    <link href="{{ asset('theme/admin/assets/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet"
-        type="text/css" />
-
-    <!--Swiper slider css-->
-    <link href="{{ asset('theme/admin/assets/libs/swiper/swiper-bundle.min.css') }}" rel="stylesheet" type="text/css" />
-
-    <!-- Layout config Js -->
-    <script src="{{ asset('theme/admin/assets/js/layout.js') }}"></script>
-    <!-- Bootstrap Css -->
-    <link href="{{ asset('theme/admin/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{ asset('theme/admin/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    <link href="{{ asset('theme/admin/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- custom Css-->
-    <link href="{{ asset('theme/admin/assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
-</head>
-
+@include('admin/includes/head')
 
 <body>
 
--- routes/web.php
+++ routes/web.php
@@ -63,9 +63,39 @@ Route::get('/about' , function(){
 });
 
 
+Route::prefix('admin')->middleware([CheckRoleAdminMiddleware::class])->group(function () {
+    // Route cho quản lý danh mục
+    Route::prefix('categories')->group(function () {
+        // Route::get('/', [CategoryController::class, 'index'])->name('admin.categories.index');
+        // Route::get('{id}', [CategoryController::class, 'show'])->name('admin.categories.show');
+        // Route::get('create', [CategoryController::class, 'create'])->name('admin.categories.create');
+        // Route::post('create', [CategoryController::class, 'store'])->name('admin.categories.store');
+        // Route::get('{id}/edit', [CategoryController::class, 'edit'])->name('admin.categories.edit');
+        // Route::put('{id}', [CategoryController::class, 'update'])->name('admin.categories.update');
+        // Route::delete('{id}', [CategoryController::class, 'destroy'])->name('admin.categories.destroy');
+    });
+
+    // Route cho quản lý sản phẩm
+    Route::prefix('products')->group(function () {
+        // Route::get('/', [ProductController::class, 'index'])->name('admin.products.index');
+        // Route::get('{id}', [ProductController::class, 'show'])->name('admin.products.show');
+        // Route::get('create', [ProductController::class, 'create'])->name('admin.products.create');
+        // Route::post('create', [ProductController::class, 'store'])->name('admin.products.store');
+        // Route::get('{id}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
+        // Route::put('{id}', [ProductController::class, 'update'])->name('admin.products.update');
+        // Route::delete('{id}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
+    });
+});
 Route::get('admin',function(){
     return view('admin.index');
 })->middleware(CheckRoleAdminMiddleware::class);
 Route::get('staff',function(){
     return view('staff.index');
 })->middleware(CheckRoleStaffMiddleware::class);
+
+Route::prefix('staff')->middleware([CheckRoleStaffMiddleware::class])->group(function () {
+    // Route cho quản lý banner
+    Route::prefix('categories')->group(function () {
+       
+    });
+});
