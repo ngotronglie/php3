a31326548f27c77f70d63e8e4f42464861036b57
NGO TRONG LIEM <ngotrongliem2004@gmail.com>
2024-08-04 11:40:47 +0700

    khoi tao quan li user

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminDanhMucController;
-use App\Http\Controllers\Admin\AdminSanPhamController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\CheckRoleAdminMiddleware;
 use App\Http\Middleware\CheckRoleStaffMiddleware;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminUserController;
+use App\Http\Controllers\Admin\AdminDanhMucController;
+use App\Http\Controllers\Admin\AdminSanPhamController;
 
 
 
@@ -30,39 +31,39 @@ Route::get('/', function () {
 /**
  * client view 
  */
-Route::get('/login',[AuthController::class, 'ShowViewLogin'] );
-Route::post('/login',[AuthController::class, 'LoginRequest'] )->name('login');
-Route::post('/register',[AuthController::class, 'RegisterRequest'] )->name('register');
-Route::get('/register',[AuthController::class, 'ShowviewRegister'] );
-Route::post('/logout',[AuthController::class, 'logout'] )->name('logout');
+Route::get('/login', [AuthController::class, 'ShowViewLogin']);
+Route::post('/login', [AuthController::class, 'LoginRequest'])->name('login');
+Route::post('/register', [AuthController::class, 'RegisterRequest'])->name('register');
+Route::get('/register', [AuthController::class, 'ShowviewRegister']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 /**
  * pages view
  */
 
-Route::get('/contact' , function(){
+Route::get('/contact', function () {
     return view('client.pages.contact');
 });
-Route::get('/blog' , function(){
+Route::get('/blog', function () {
     return view('client.pages.blog');
 });
-Route::get('/blog-detail' , function(){
+Route::get('/blog-detail', function () {
     return view('client.pages.blog-detail');
 });
-Route::get('/shop' , function(){
+Route::get('/shop', function () {
     return view('client.pages.shop');
 });
-Route::get('/wishlist' , function(){
+Route::get('/wishlist', function () {
     return view('client.pages.wishlist');
 });
-Route::get('/cart' , function(){
+Route::get('/cart', function () {
     return view('client.pages.cart');
 });
-Route::get('/checkout' , function(){
+Route::get('/checkout', function () {
     return view('client.pages.checkout');
 });
 
-Route::get('/about' , function(){
+Route::get('/about', function () {
     return view('client.pages.about');
 });
 
@@ -70,13 +71,13 @@ Route::get('/about' , function(){
 Route::prefix('admin')->middleware([CheckRoleAdminMiddleware::class])->group(function () {
     // Route cho quản lý danh mục
     Route::prefix('categories')->group(function () {
-        Route::get('/' , [AdminDanhMucController::class, 'index' ])->name('admin.categories.list');
-        Route::get('/create' , [AdminDanhMucController::class, 'create' ])->name('admin.categories.create');
-        Route::post('/store' , [AdminDanhMucController::class, 'store' ])->name('admin.categories.store');
-        Route::get('/show/{id}' , [AdminDanhMucController::class, 'show' ])->name('admin.categories.show');
-        Route::get('/{id}/edit' , [AdminDanhMucController::class, 'edit' ])->name('admin.categories.edit');
-        Route::put('/{id}/update' , [AdminDanhMucController::class, 'update' ])->name('admin.categories.update');
-        Route::delete('/{id}/destroy' , [AdminDanhMucController::class, 'destroy' ])->name('admin.categories.destroy');
+        Route::get('/', [AdminDanhMucController::class, 'index'])->name('admin.categories.list');
+        Route::get('/create', [AdminDanhMucController::class, 'create'])->name('admin.categories.create');
+        Route::post('/store', [AdminDanhMucController::class, 'store'])->name('admin.categories.store');
+        Route::get('/show/{id}', [AdminDanhMucController::class, 'show'])->name('admin.categories.show');
+        Route::get('/{id}/edit', [AdminDanhMucController::class, 'edit'])->name('admin.categories.edit');
+        Route::put('/{id}/update', [AdminDanhMucController::class, 'update'])->name('admin.categories.update');
+        Route::delete('/{id}/destroy', [AdminDanhMucController::class, 'destroy'])->name('admin.categories.destroy');
     });
 
     // Route cho quản lý sản phẩm
@@ -89,17 +90,28 @@ Route::prefix('admin')->middleware([CheckRoleAdminMiddleware::class])->group(fun
         Route::put('/{id}/update', [AdminSanPhamController::class, 'update'])->name('admin.products.update');
         Route::delete('{id}/destroy', [AdminSanPhamController::class, 'destroy'])->name('admin.products.destroy');
     });
+
+
+    // route quan li user 
+    Route::prefix('User')->group(function () {
+        Route::get('/', [AdminUserController::class, 'index'])->name('admin.users.list');
+        Route::get('/show/{id}', [AdminUserController::class,  'show'])->name('admin.users.show');
+        Route::get('/create', [AdminUserController::class,  'create'])->name('admin.users.create');
+        Route::post('/store', [AdminUserController::class,  'store'])->name('admin.users.store');
+        Route::get('{id}/edit', [AdminUserController::class, 'edit'])->name('admin.users.edit');
+        Route::put('/{id}/update', [AdminUserController::class,  'update'])->name('admin.users.update');
+        Route::delete('{id}/destroy', [AdminUserController::class,  'destroy'])->name('admin.users.destroy');
+    });
 });
-Route::get('admin',function(){
+Route::get('admin', function () {
     return view('admin.includes.main');
 })->middleware(CheckRoleAdminMiddleware::class);
-Route::get('staff',function(){
+Route::get('staff', function () {
     return view('staff.index');
 })->middleware(CheckRoleStaffMiddleware::class);
 
 Route::prefix('staff')->middleware([CheckRoleStaffMiddleware::class])->group(function () {
     // Route cho quản lý banner
     Route::prefix('categories')->group(function () {
-       
     });
 });
